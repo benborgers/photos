87eddb0be4d31cd30419160bbdb1b3fbf0ba373c
Ben Borgers <borgersbenjamin@gmail.com>
2024-05-15 18:11:33 +0200

    Endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/photos', Controllers\GetPhotos::class);
