a1e7106010ec787794bf0441eb979d1ff0f4aefd
Ben Borgers <borgersbenjamin@gmail.com>
2024-05-18 12:29:53 -0400

    Make storage links https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
