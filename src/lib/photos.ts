4eadbdd3c9ddf0153039bd68eadba4fe072d2127
Ben Borgers <borgersbenjamin@gmail.com>
2025-01-07 01:59:22 -0500

    Handle .mov

--- src/lib/photos.ts
+++ src/lib/photos.ts
@@ -5,12 +5,15 @@ export function getPhotoFiles(): Record<string, any> {
 export function getPhotos(): {
   filename: string;
   ymd: string;
+  extension: string;
 }[] {
   return Object.entries(getPhotoFiles())
-    .map(([path, value]) => {
+    .map(([path]) => {
+      const extension = path.split(".")[1].toLowerCase();
       return {
         filename: path,
         ymd: path.split("/").reverse()[0].split(".")[0],
+        extension,
       };
     })
     .sort((a, b) => {
