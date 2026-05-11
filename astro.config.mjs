1731f778cad693088657eaf234aabc8d9f1aefcf
Ben Borgers <borgersbenjamin@gmail.com>
2026-05-10 21:27:24 -0400

    Migrate Tailwind setup for Astro 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   devToolbar: {
     enabled: false,
